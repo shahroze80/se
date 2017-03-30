f3a8096ec49699d7a1713208c83998650373642d
shahroze <shahrozetariq@hotmail.com>
2017-03-30 10:34:24 +0500

    print n fib numbers

---
+++ math.py
@@ -0,0 +1,10 @@
+# recursive implementation of nth fibionachi numbers
+def fib(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fib(n-1)+fib(n-2)
+
+
+# print first 10 fibionachi numbers
+for i in range(1,10):
+	print fib(i);
